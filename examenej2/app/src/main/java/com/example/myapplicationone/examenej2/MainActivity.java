cd0b91377115dfa4bdc276962d2264cdd4e95c39
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-05-08 00:38:08 -0500

    feat: vista principal y logica en el MainActivity

--- examenej2/app/src/main/java/com/example/myapplicationone/examenej2/MainActivity.java
+++ examenej2/app/src/main/java/com/example/myapplicationone/examenej2/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.myapplicationone.examenej2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listViewComidas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        listViewComidas = findViewById(R.id.listViewComidas);
+
+        String[] platos = {
+                "Ceviche", "Lomo Saltado", "Pollo a la Brasa", "Ají de Gallina",
+                "Arroz con Pollo", "Causa Rellena", "Anticuchos", "Tallarines Verdes",
+                "Papa a la Huancaína", "Arroz Chaufa"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, platos);
+        listViewComidas.setAdapter(adapter);
+
+        listViewComidas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String platoElegido = platos[i];
+                Intent intent = new Intent(MainActivity.this, DetalleComidaActivity.class);
+                intent.putExtra("PLATO", platoElegido);
+                startActivity(intent);
+            }
         });
     }
 }
